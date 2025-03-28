ea2a05fe4a62fa697132e14840172030c968ddd9
Ajinkya <ajinkya0608@gmail.com>
2025-03-28 17:01:32 +0530

    added loader component

---
+++ frontend/src/ui-components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white/50 backdrop-blur-[1%] z-50">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
