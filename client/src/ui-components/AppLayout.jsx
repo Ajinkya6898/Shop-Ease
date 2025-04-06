f4bd9c793bfe468cd1073d5e94037fd000018bbf
Ajinkya <ajinkya0608@gmail.com>
2025-04-06 19:42:57 +0530

    small ui changes

--- client/src/ui-components/AppLayout.jsx
+++ client/src/ui-components/AppLayout.jsx
@@ -7,15 +7,12 @@ import Loader from "./Loader";
 const AppLayout = () => {
   const { state } = useNavigation();
 
-  if (state === "loading") {
-    return <Loader />;
-  }
-
   return (
     <div className="grid grid-cols-[26rem_1fr] grid-rows-[auto_1fr] h-screen">
       <Header />
       <Sidebar />
       <main className="bg-gradient-to-br to-brand-100 py-12 px-[4.8rem] overflow-y-scroll ">
+        {state === "loading" && <Loader />}
         <Outlet />
       </main>
     </div>
